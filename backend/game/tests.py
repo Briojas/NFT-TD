9f3a7bb0cb18f57425719b623734b5d2272998d8
Josiah Lund <josiahlund@gmail.com>
2023-05-19 23:01:03 -0700

    Add tests to for validation logic into a decorator
    Theis should eventually be made more generic. This is a transition step.

--- backend/game/tests.py
+++ backend/game/tests.py
@@ -1,10 +1,23 @@
 from django.test import TestCase
 from hypothesis import given, strategies as st
+import pytest
 
 from game import models
 
 
-def test_inclusive_range(start=1, stop=10):
+def test_validation_wrapper():
+    card = models.Card(tier=2, priority=5)
+    assert card.tier == 2
+    assert card.priority == 5
+
+
+def test_validation_wrapper_invalid_inputs():
+    with pytest.raises(ValueError) as e_info:
+        card = models.Card(tier=5, priority=5)
+    assert "Tier must be an integer between 1 and 3, inclusive" in str(e_info.value)
+
+
+def test_inclusive_range(start: int=1, stop: int=10):
     exclusive_range = range(start, stop)
     inclusive_range = models._inclusive_range(start, stop)
     assert min(inclusive_range) == min(exclusive_range)
