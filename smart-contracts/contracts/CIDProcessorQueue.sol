6e2021a026b16d03e4f7c60afd24f8a119d32b6f
Briojas <blake.a.riojas@gmail.com>
2023-05-22 21:16:49 -0500

    cid processor queue lib

---
+++ smart-contracts/contracts/CIDProcessorQueue.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+    //using the "Iterable Mappings" Solidity example
+library CIDProcessorQueue {
+    enum State {READY, VERIFYING, VERIFIED, ASSESSED}
+
+    struct Submission {
+        address payable user;
+        uint key_index; //storage position in the queue array
+        uint ticket; //position in the queue for execution
+        string cid; //ipfs cid of the item being processed
+        State state; //status of the submission
+    }
+
+    struct Key_Flag { 
+        uint key; 
+        bool deleted;
+    }
+
+    struct Tickets {
+        uint num_tickets;
+        uint curr_ticket;
+        uint curr_ticket_key;
+        uint next_submission_key;
+    }
+
+    struct Queue { 
+        mapping(uint => Submission) data;
+        Key_Flag[] keys;
+        Tickets tickets;
+        State state;
+    }
+    
+    type Iterator is uint;
+
+    function initiate(Queue storage self) internal {
+        self.tickets.num_tickets = 0; //will increment to 1 after first Submission
+        self.tickets.curr_ticket = 0; //first ticket submitted will be ticket 1
+        self.tickets.next_submission_key = 0; //first submission is placed at beginning of queue
+        self.state = State.READY;
+    }
+
+    function join(Queue storage self, string calldata cid) internal{
+        uint key = self.tickets.next_submission_key;
+        uint key_index = self.data[key].key_index;
+        self.tickets.num_tickets ++;
+        self.data[key].ticket = self.tickets.num_tickets;
+        self.data[key].cid = cid;
+        self.data[key].state = State.READY;
+        if (key_index > 0){ //checks if key already existed, and was overwritten
+            self.keys[key_index].deleted = false; //a deleted Submission overwritten should not be marked deleted
+        } else { //if key didn't exist, the queue array has grown
+            key_index = self.keys.length;
+            self.keys.push();
+            self.data[key].key_index = key_index + 1;
+            self.keys[key_index].key = key;
+        }
+        set_next_sub_key(self);
+    }
+
+    function remove(Queue storage self, uint key) internal returns (bool success) {
+        uint key_index = self.data[key].key_index;
+        if (key_index == 0)
+            return false;
+        delete self.data[key];
+        self.keys[key_index - 1].deleted = true;
+        return true;
+    }
+
+    function set_next_sub_key(Queue storage self) internal {
+        if(self.keys.length < 0){
+            self.tickets.next_submission_key = 0;
+        }
+        self.tickets.next_submission_key = Iterator.unwrap(iterator_find_next_deleted(self, 0));
+    }
+
+    function find_ticket_key(Queue storage self, uint ticket) internal view returns (uint goal_key){
+        for(
+            Iterator key = iterate_start(self);
+            iterate_valid(self, key);
+            key = iterate_next(self, key)
+        ){
+            if(ticket == self.data[Iterator.unwrap(key)].ticket){
+                goal_key =  Iterator.unwrap(key);
+            }
+        }
+    }
+
+    function set_curr_ticket_key(Queue storage self) internal {
+        self.tickets.curr_ticket_key = find_ticket_key(self, self.tickets.curr_ticket);
+    }
+
+    function update_state(Queue storage self) internal {
+        if(self.state == State.ASSESSED){
+            self.state = State.READY;
+        }else{
+            self.state = State(uint(self.state) + 1);
+        }
+    }
+
+    function set_sub_state(Queue storage self, State state) internal {
+        self.data[self.tickets.curr_ticket_key].state = state;
+    }
+
+    function iterate_start(Queue storage self) internal view returns (Iterator) {
+        return iterator_skip_deleted(self, 0);
+    }
+
+    function iterate_valid(Queue storage self, Iterator iterator) internal view returns (bool) {
+        return Iterator.unwrap(iterator) < self.keys.length;
+    }
+
+    function iterate_next(Queue storage self, Iterator iterator) internal view returns (Iterator) {
+        return iterator_skip_deleted(self, Iterator.unwrap(iterator) + 1);
+    }
+
+    function iterator_skip_deleted(Queue storage self, uint key_index) internal view returns (Iterator) {
+        while (key_index < self.keys.length && self.keys[key_index].deleted)
+            key_index++;
+        return Iterator.wrap(key_index);
+    }
+    
+    function iterator_find_next_deleted(Queue storage self, uint key_index) internal view returns (Iterator) {
+        while (key_index < self.keys.length && !self.keys[key_index].deleted)
+            key_index++;
+        return Iterator.wrap(key_index);
+    }
+
+    event ticket_assigned(
+        address user,
+        uint ticket,
+        uint ticket_key,
+        string cid
+    );
+}
